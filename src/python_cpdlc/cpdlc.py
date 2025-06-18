ec2be576d97edd7d6ffe67be1abf23a14ba5539f
Half_nothing <Half_nothing@163.com>
2025-06-19 00:06:39 +0800

    feat: Add logger for debug and add custom url

--- src/python_cpdlc/cpdlc.py
+++ src/python_cpdlc/cpdlc.py
@@ -1,6 +1,7 @@
 from typing import Callable, Optional
 
 from bs4 import BeautifulSoup
+from loguru import logger
 from requests import post
 
 from .acars_message import AcarsMessage
@@ -27,8 +28,8 @@ def parser_message(text: str) -> list["AcarsMessage"]:
 
 
 class CPDLC:
-    def __init__(self, email: str, login_code: str, *,
-                 cpdlc_connect_callback: Optional[Callable[[str, str], None]] = None,
+    def __init__(self, email: str, login_code: str, acars_url: str = "http://www.hoppie.nl/acars/system", *,
+                 cpdlc_connect_callback: Optional[Callable[[], None]] = None,
                  cpdlc_disconnect_callback: Optional[Callable[[], None]] = None):
         self.login_code = login_code
         self.email = email
@@ -41,31 +42,43 @@ class CPDLC:
         self.cpdlc_atc_callsign: Optional[str] = None
         self.cpdlc_connect_callback = cpdlc_connect_callback
         self.cpdlc_disconnect_callback = cpdlc_disconnect_callback
+        self.acars_url = acars_url
+        logger.debug(f"CPDLC init complete. Connection OK. Current network: {self.network.value}")
 
     def add_message_callback(self, callback: Callable[[AcarsMessage], None]) -> None:
         self.callback.append(callback)
 
     async def start_poller(self):
+        logger.debug(f"Poll thread started")
         await self.poller.start()
 
+    def _cpdlc_logout(self):
+        self.cpdlc_connect = False
+        self.cpdlc_current_atc = None
+        self.cpdlc_atc_callsign = None
+        logger.debug(f"CPDLC disconnected")
+        self.cpdlc_disconnect_callback()
+
     def handle_message(self, message: AcarsMessage):
-        print(message)
+        logger.debug(f"Received message: {message}")
         if isinstance(message, CPDLCMessage):
-            if message.message.startswith("CURRENT ATC UNIT"):
-                # cpdlc connect message
+            if message.message == "LOGON ACCEPTED":
+                # cpdlc logon success
                 self.cpdlc_connect = True
+                logger.success(f"CPDLC connected. ATC Unit: {self.cpdlc_current_atc}")
+                if self.cpdlc_connect_callback is not None:
+                    self.cpdlc_connect_callback()
+            if message.message.startswith("CURRENT ATC UNIT"):
+                # cpdlc atc info
                 info = message.message.split("@_@")
                 self.cpdlc_current_atc = info[1]
                 self.cpdlc_atc_callsign = info[2]
-                print(f"CPDLC connected\nCurrent ATC: {self.cpdlc_current_atc}\nCallsign: {self.cpdlc_atc_callsign}")
+                logger.success(f"ATC Unit: {self.cpdlc_current_atc}. Callsign: {self.cpdlc_atc_callsign}")
             if message.message == "LOGOFF":
-                self.cpdlc_connect = False
-                self.cpdlc_current_atc = None
-                self.cpdlc_atc_callsign = None
-                print(f"CPDLC disconnected")
+                self._cpdlc_logout()
 
     def poll_message(self):
-        res = post("https://www.hoppie.nl/acars/system/connect.html", {
+        res = post(f"{self.acars_url}/connect.html", {
             "logon": self.login_code,
             "from": self.callsign,
             "to": "SERVER",
@@ -78,7 +91,7 @@ class CPDLC:
                 callback(message)
 
     def get_network(self) -> Network:
-        res = post("https://www.hoppie.nl/acars/system/account.html", {
+        res = post(f"{self.acars_url}/account.html", {
             "email": self.email,
             "logon": self.login_code
         })
@@ -91,30 +104,38 @@ class CPDLC:
         return Network(selected.text)
 
     def change_network(self, new_network: Network):
-        post("https://www.hoppie.nl/acars/system/account.html", {
+        post(f"{self.acars_url}/account.html", {
             "email": self.email,
             "logon": self.login_code,
             "network": new_network.value
         })
+        logger.debug(f"Network changed to {new_network.value}")
 
     def set_callsign(self, callsign: str):
         self.callsign = callsign
 
-    def ping_station(self, station_callsign: str = "SERVER") -> None:
+    def ping_station(self, station_callsign: str = "SERVER") -> bool:
         if self.callsign is None:
             raise CallsignError()
-        post("http://www.hoppie.nl/acars/system/connect.html", {
+        logger.debug(f"Ping station: {station_callsign}")
+        res = post(f"{self.acars_url}/connect.html", {
             "logon": self.login_code,
             "from": self.callsign,
             "to": station_callsign,
             "type": PacketType.PING.value,
             "packet": ""
         })
+        if res.text != "OK":
+            logger.error(f"Ping station {station_callsign} failed")
+            return False
+        logger.debug(f"Ping station {station_callsign} succeeded")
+        return True
 
     def query_info(self, info_type: InfoType, icao: str) -> AcarsMessage:
         if self.callsign is None:
             raise CallsignError()
-        res = post("http://www.hoppie.nl/acars/system/connect.html", {
+        logger.debug(f"Query {info_type.value} for {icao}")
+        res = post(f"{self.acars_url}/connect.html", {
             "logon": self.login_code,
             "from": self.callsign,
             "to": "SERVER",
@@ -123,31 +144,66 @@ class CPDLC:
         })
         return parser_message(res.text)[0]
 
-    def departure_clearance_delivery(self, target_station: str, aircraft_type: str, dest_airport: str, dep_airport: str,
-                                     stand: str, atis_letter: str) -> None:
-        post("http://www.hoppie.nl/acars/system/connect.html", {
+    def send_telex_message(self, target_station: str, message: str) -> bool:
+        if self.callsign is None:
+            raise CallsignError()
+        logger.debug(f"Send telex message {message}")
+        res = post(f"{self.acars_url}/connect.html", {
             "logon": self.login_code,
             "from": self.callsign,
             "to": target_station.upper(),
             "type": PacketType.TELEX.value,
-            "packet": f"REQUEST PREDEP CLEARANCE {self.callsign} {aircraft_type} "
-                      f"TO {dest_airport.upper()} AT {dep_airport.upper()} STAND {stand} ATIS {atis_letter}"
+            "packet": message
         })
+        return res.text == "ok"
 
-    def reply_cpdlc_message(self, message: CPDLCMessage, status: bool) -> None:
-        post("http://www.hoppie.nl/acars/system/connect.html", {
+    def departure_clearance_delivery(self, target_station: str, aircraft_type: str, dest_airport: str, dep_airport: str,
+                                     stand: str, atis_letter: str) -> bool:
+        if self.callsign is None:
+            raise CallsignError()
+        logger.debug(f"Send DCL to {target_station} from {dep_airport} to {dest_airport}")
+        return self.send_telex_message(target_station,
+                                       f"REQUEST PREDEP CLEARANCE {self.callsign} {aircraft_type} "
+                                       f"TO {dest_airport.upper()} AT {dep_airport.upper()} STAND {stand} "
+                                       f"ATIS {atis_letter}")
+
+    def reply_cpdlc_message(self, message: CPDLCMessage, status: bool) -> bool:
+        if self.callsign is None:
+            raise CallsignError()
+        logger.debug(f"Reply CPDLC message with status {status}")
+        res = post(f"{self.acars_url}/connect.html", {
             "logon": self.login_code,
             "from": self.callsign,
             "to": message.from_station,
             "type": PacketType.CPDLC.value,
             "packet": message.reply_message(status)
         })
+        return res.text == "ok"
 
-    def cpdlc_login(self, target_station: str) -> None:
-        post("http://www.hoppie.nl/acars/system/connect.html", {
+    def cpdlc_login(self, target_station: str) -> bool:
+        if self.callsign is None:
+            raise CallsignError()
+        logger.debug(f"CPDLC login to {target_station}")
+        self.cpdlc_current_atc = target_station.upper()
+        res = post(f"{self.acars_url}/connect.html", {
             "logon": self.login_code,
             "from": self.callsign,
             "to": target_station,
             "type": PacketType.CPDLC.value,
             "packet": f"/data2/{message_id_manager.next_message_id()}//Y/REQUEST LOGON"
         })
+        return res.text == "ok"
+
+    def cpdlc_logout(self) -> bool:
+        if self.callsign is None:
+            raise CallsignError()
+        logger.debug(f"CPDLC logout")
+        res = post(f"{self.acars_url}/connect.html", {
+            "logon": self.login_code,
+            "from": self.callsign,
+            "to": self.cpdlc_current_atc,
+            "type": PacketType.CPDLC.value,
+            "packet": f"/data2/{message_id_manager.next_message_id()}//N/LOGOFF"
+        })
+        self._cpdlc_logout()
+        return res.text == "ok"
